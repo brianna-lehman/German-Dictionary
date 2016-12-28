882f1754826940df99f126a779ee2045f4814b1d
brianna-lehman <bml49@pitt.edu>
2016-12-28 16:55:29 -0500

    adding Dictionary class code

---
+++ Dictionary.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Dictionary implements Serializable{
+	// an array list of German objects which can be sorted by the english word, the german word, the gender, or the quality
+
+	ArrayList<German> dict; 
+
+	public Dictionary() {
+		dict = new ArrayList<German>();
+	}
+
+	public void sort(String mode) {
+		// NOTE: make sure that the arraylist uses a stable sort
+
+		if (mode.equalsIgnoreCase("english")) {
+			// sort the objects in the dictionary by the english word from the German object
+			Collections.sort(dict, German.EngComparator); 
+		} else if (mode.equalsIgnoreCase("german")) {
+			// sort the objects in the dictionary by the german word from the German object
+			Collections.sort(dict, German.GerComparator);
+		} else if (mode.equalsIgnoreCase("gender")) {
+			// sort the objects in the dictionary by the gender from the German object
+			Collections.sort(dict, German.GenderComparator);
+		} else if (mode.equalsIgnoreCase("quality")) {
+			// sor the objects in the dictionary by the quality from the German object
+			Collections.sort(dict, German.QualityComparator);
+		} else {
+			System.out.println("Error, unable to sort by this parameter.");
+		}
+	}
+
+	public void printList() {
+		for (German entry: dict) {
+			System.out.println(entry.getEnglish()+": ("+entry.getGender()+") "+entry.getGerman()+", "+entry.getPlural()+", "+entry.getQuality());
+		}
+	}
+
+	public void add(German entry) {
+		dict.add(entry);
+	}
+}
--- GermanDriver.java
+++ GermanDriver.java
@@ -2,16 +2,16 @@ import java.util.*;
 import java.io.*;
 
 public class GermanDriver {
-	public static ArrayList<German> dict = null;
 
 	public static void main(String[] args) {
+		Dictionary dict = null;
 		ObjectInputStream ois = null;
 		ObjectOutputStream oos = null;
 
 		// a dictionary hasn't been created yet
 		if (args.length == 0) {
 			// create a new dictionary
-			dict = new ArrayList<German>();
+			dict = new Dictionary();
 
 			try {
 				// create a new file that will hold the dictionary object
@@ -26,7 +26,7 @@ public class GermanDriver {
 			/* open this file, create a dictionary out of the ArrayList object read in from the file */
 			try {
 				ois = new ObjectInputStream(new FileInputStream(args[0]));
-				dict = (ArrayList<German>)ois.readObject();
+				dict = (Dictionary)ois.readObject();
 				ois.close();
 			} catch (Exception ex) {
 				System.out.println("Error opening file and reading dictionary object.");
@@ -44,20 +44,20 @@ public class GermanDriver {
 
 		while (choice > 0) {
 			if (choice == 1) {
-				sort("english");
-				printList();
+				dict.sort("english");
+				dict.printList();
 				choice = printMenu();
 			} else if (choice == 2) {
-				sort("german");
-				printList();
+				dict.sort("german");
+				dict.printList();
 				choice = printMenu();
 			} else if (choice == 3) {
-				sort("gender"); 
-				printList();
+				dict.sort("gender"); 
+				dict.printList();
 				choice = printMenu();
 			} else if (choice == 4) {
-				sort("quality");
-				printList();
+				dict.sort("quality");
+				dict.printList();
 				choice = printMenu();
 			} else if (choice == 5) {
 				Scanner kb = new Scanner(System.in);
@@ -108,31 +108,5 @@ public class GermanDriver {
 		System.out.println("6.\tExit");
 
 		return kb.nextInt();
-	}
-
-	public static void sort(String mode) {
-		// NOTE: make sure that the arraylist uses a stable sort
-
-		if (mode.equalsIgnoreCase("english")) {
-			// sort the objects in the dictionary by the english word from the German object
-			Collections.sort(dict, German.EngComparator); 
-		} else if (mode.equalsIgnoreCase("german")) {
-			// sort the objects in the dictionary by the german word from the German object
-			Collections.sort(dict, German.GerComparator);
-		} else if (mode.equalsIgnoreCase("gender")) {
-			// sort the objects in the dictionary by the gender from the German object
-			Collections.sort(dict, German.GenderComparator);
-		} else if (mode.equalsIgnoreCase("quality")) {
-			// sor the objects in the dictionary by the quality from the German object
-			Collections.sort(dict, German.QualityComparator);
-		} else {
-			System.out.println("Error, unable to sort by this parameter.");
-		}
-	}
-
-	public static void printList() {
-		for (German entry: dict) {
-			System.out.println(entry.getEnglish()+": ("+entry.getGender()+") "+entry.getGerman()+", "+entry.getPlural()+", "+entry.getQuality());
-		}
 	}
 }
